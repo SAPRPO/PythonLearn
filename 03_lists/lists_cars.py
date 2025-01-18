7e6af39a3602938a8d3a07d19b8dd4d4a91110d4
Ilia Uryupin <uis071287@gmail.com>
2025-01-18 06:49:22 +0300

    lists sorting learn

---
+++ 03_lists/lists_cars.py
@@ -0,0 +1,27 @@
+cars = ['bmw', 'audi', 'mercedes', 'toyota', 'honda', 'chevrolet']
+#sort 
+cars.sort()
+print(cars)
+#sort reverse
+cars.sort(reverse=True)
+print(cars)
+print("\ntemp sort sorted()")
+#temp sort sorted()
+cars = ['bmw', 'audi', 'mercedes', 'toyota', 'honda', 'chevrolet']
+print("\nOriginal carlist")
+print(cars)
+
+print("n\Sorted carlist")
+print(sorted(cars))
+
+print("\nOriginal carlist")
+print(cars)
+
+print("\n lists reverse")
+
+cars.reverse()
+print(cars)
+
+print("\nList Length")
+
+print(len(cars))
