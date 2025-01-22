6ced9adcc1ffccea34063b6acdaeef9a345806b6
Ilia Uryupin <uis071287@gmail.com>
2025-01-22 05:28:08 +0300

    fixed extension

---
+++ 04_work_with_lists/04_work1.py
@@ -0,0 +1,9 @@
+print("\ncyrcle for \n")
+magicians= ['alice','david','carolina']
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick, {magician.title()}\n")
+print("\nThanks for everyone!!!\n")
+
+
+
