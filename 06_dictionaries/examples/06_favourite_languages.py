0c9610278965094ac62ae88e12425e4d7499aa5f
Ilya Uryupin <uis071287@gmail.com>
2025-02-21 16:53:35 +0300

    update examples with dictionaries

--- 06_dictionaries/examples/06_favourite_languages.py
+++ 06_dictionaries/examples/06_favourite_languages.py
@@ -7,3 +7,25 @@ favourite_languages = {
 
 language = favourite_languages['sarah'].title()
 print(f"Sarah's favourite language is {language}")
+
+for name, lang in favourite_languages.items():
+    print(f"{name.title()} favourite language is {lang.title()}")
+
+    #keys in dictionary
+print("\n")
+
+for name in favourite_languages.keys(): # for name in favourite_languages 
+    print(name.title())
+
+print("\n--------------------")
+
+friends = ['phil', 'sarah']
+
+for name in favourite_languages.keys():
+    print(f"Hi {name.title()}")
+    if name in friends:
+        language = favourite_languages[name].title() # value of key
+        print(f"\t{name.title()}, I see you love {language}")
+        
+    if 'erin' not in favourite_languages.keys():
+        print("Erin, please take our poll!")
---
+++ 06_dictionaries/examples/06_favourite_languages_sort_values.py
@@ -0,0 +1,34 @@
+favourite_languages = {
+'jen' : 'python',
+'sarah' : 'с',
+'edward' : 'rust',
+'phil' : ' python',
+}
+
+
+print("\n--------------------")
+
+#friends = ['phil', 'sarah']
+
+#for name in favourite_languages.keys():
+#    print(f"Hi {name.title()}")
+#    if name in friends:
+#        language = favourite_languages[name].title() # value of key
+#        print(f"\t{name.title()}, I see you love {language}")
+        
+#    if 'erin' not in favourite_languages.keys():
+#        print("Erin, please take our poll!")
+
+for name in sorted(favourite_languages.keys()):
+   print(f"{name.title()}, thank you for taking the poll.")
+
+print("\n--------------------")
+
+print("The following languages have been mentioned:")
+for language in favourite_languages.values():
+    print(language.title())
+
+# dublicates
+print ( "'The following languages have been mentioned: ")
+for language in set(favourite_languages.values()):
+    print(language.title())
