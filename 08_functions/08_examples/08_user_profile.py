d6d874cc755a3e1941fce4828220dd31ccbd01d4
Ilya Uryupin <i.uryupin@module.ru>
2025-03-12 18:00:50 +0300

    add examples for 08

---
+++ 08_functions/08_examples/08_user_profile.py
@@ -0,0 +1,7 @@
+def build_profile(first,last,**user_info):  #**user_info - empty dictionary
+    user_info['first_name']=first
+    user_info['last_name']=last
+    return user_info
+
+user_profile=build_profile('albert','einstein',location='princeton',field='physics', sex='male')
+print (user_profile)
