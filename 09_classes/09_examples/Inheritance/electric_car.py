003432ec98292fa1d321ed7a1329f5088dedd8b0
Ilya Uryupin <i.uryupin@module.ru>
2025-03-21 13:07:09 +0300

    update example electric_car.py

--- 09_classes/09_examples/Inheritance/electric_car.py
+++ 09_classes/09_examples/Inheritance/electric_car.py
@@ -5,6 +5,7 @@ class Car:
         self.model = model
         self.year = year
         self.odometer_reading = 0
+        self.gas_tank = 10
     
     def get_descriptive_name(self):
         long_name = f"{self.year} {self.make} {self.model}"
@@ -21,22 +22,56 @@ class Car:
 
     def increment_odometer(self,miles):
         self.odometer_reading+=miles
+        
+    
+    def fill_gas_tank(self, fuel):
+        if fuel >= self.gas_tank:
+            self.gas_tank = fuel
+            print(f"Gas tank is filled with {self.gas_tank} litres")
+        else:
+            print ("Fuel must be greater than the current amount of gas")
 
+#class as attribute
+
+class Battery:
+    def __init__(self, battery_size=40):
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        print(f"This car has a {self.battery_size}-kWh battery")
+
+    def get_range(self):
+        if self.battery_size == 40:
+            range = 150
+        elif self.battery_size == 65:
+            range = 225
+
+        print(f"This car can go about {range} miles on a full charge")
 
 class ElectricCar(Car):
     def __init__(self,make,model,year):
         super().__init__(make,model,year) #конструктор базового  класса (суперкласса). Потомок  -подкласс
         #attributes of ElectricCar
-        self.battery_size = 75
+        #self.battery_size=40
+        self.battery = Battery()
+
     
     def describe_battery(self):
         print(f"This car has a {self.battery_size}-kWh battery.")
+
+    def fill_gas_tank(self):
+        
+        print("This car doesn't need a gas tank!")
         
 my_el_car = ElectricCar("Tesla","Model S",2020)
 print(my_el_car.get_descriptive_name())
-my_el_car.describe_battery()
+my_el_car.fill_gas_tank() #override the parent method without passing parameters
+my_el_car.battery.describe_battery()
+my_el_car.battery.get_range()
 
-#207
+
+
+
 
 #my_new_car = Car("audi","a4",2019)
 #print(my_new_car.get_descriptive_name())
