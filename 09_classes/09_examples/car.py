fea16d1dc49c1129f245248619868e94960c3602
Ilya Uryupin <i.uryupin@module.ru>
2025-05-16 16:38:12 +0300

    modified car.py

--- 09_classes/09_examples/car.py
+++ 09_classes/09_examples/car.py
@@ -23,6 +23,39 @@ class Car:
     def increment_odometer(self,miles):
         self.odometer_reading+=miles
 
+class Battery:
+    def __init__(self, battery_size=40):
+        self.battery_size = battery_size
 
+    def describe_battery(self):
+        print(f"This car has a {self.battery_size}-kWh battery")
+
+    def get_range(self):
+        if self.battery_size == 40:
+            range = 150
+            
+        elif self.battery_size == 65:
+            range = 225
+        #return range 
+        print(f"This car can go about {range} miles on a full charge")   
+
+    def upgrade_battery(self):
+        self.battery_size = 65
+
+        print(f"Battery upgraded. This car can go about {self.get_range()} miles on a full charge. Battery size = {self.battery_size}")
        
     
+class ElectricCar(Car):
+    def __init__(self,make,model,year):
+        super().__init__(make,model,year) #конструктор базового  класса (суперкласса). Потомок  -подкласс
+        #attributes of ElectricCar
+        #self.battery_size=40
+        self.battery = Battery()
+
+    
+    def describe_battery(self):
+        print(f"This car has a {self.battery_size}-kWh battery.")
+
+    def fill_gas_tank(self):
+        
+        print("This car doesn't need a gas tank!")
