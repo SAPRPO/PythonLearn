674db8b01f5ef8161dd8c7ecbc09d96dd690c4ac
Ilya Uryupin <i.uryupin@module.ru>
2025-05-23 17:57:21 +0300

    add standart_library_test

---
+++ 09_classes/09_examples/standart_library/stlib_test.py
@@ -0,0 +1,9 @@
+from random import randint
+from random import choice
+d = randint(1, 9)
+print(f'Random digit: {d}')
+
+meal =['MEAT', 'salad',  'spagetty',  'meatballs',  'VODKA']
+
+random_product = choice(meal)
+print(random_product)
