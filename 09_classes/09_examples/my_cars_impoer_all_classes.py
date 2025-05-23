735f4bd967d4301151a5545a0171c27a8f9b99c9
Ilya Uryupin <i.uryupin@module.ru>
2025-05-23 17:11:42 +0300

    delete old

--- 09_classes/09_examples/my_cars_impoer_all_classes.py
+++
@@ -1,6 +0,0 @@
-from car import *
-
-my_car1 = Car('ford', 'mustang', 2024)
-print(my_car1.get_descriptive_name())
-my_car2 = ElectricCar('nissan', 'leaf', 2023)
-print(my_car2.get_descriptive_name())
--- 09_classes/09_examples/my_electric_car.py
+++
@@ -1,6 +0,0 @@
-from car import ElectricCar
-
-electric_car = ElectricCar('nissan', 'leaf', 2024)
-print(electric_car.get_descriptive_name())
-electric_car.battery.describe_battery()
-electric_car.battery.get_range()
