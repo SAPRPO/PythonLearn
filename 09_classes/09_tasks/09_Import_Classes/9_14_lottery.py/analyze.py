97050fdeaf26c57258597d052b51dd596c1d6ae7
Ilya Uryupin <i.uryupin@module.ru>
2025-05-26 11:53:35 +0300

    update lottery.py, add analyze.py

---
+++ 09_classes/09_tasks/09_Import_Classes/9_14_lottery.py/analyze.py
@@ -0,0 +1,15 @@
+from lottery import list, ticket_combination
+
+print(list)
+winner_ticket = ticket_combination()
+#print(winner_ticket)
+
+count = 1
+while(True):
+    play_result = ticket_combination()
+    if(play_result != winner_ticket):
+        print(f"Attempt: {count}, result: {play_result}")
+        count = count+1
+    else:
+        print(f"Finish! Winner ticket: {winner_ticket}, Count = {count}")
+        break
--- 09_classes/09_tasks/09_Import_Classes/9_14_lottery.py/lottery.py
+++ 09_classes/09_tasks/09_Import_Classes/9_14_lottery.py/lottery.py
@@ -1,13 +1,22 @@
 from random import choice
 
 list = [ 0, 1, 2, 3, 4, 5,6, 7,8,9 ,'z','a','b','c','d']
-res_string=""
-for r in range(0,4):
-    result = choice(list)
-    #int or not?
-    if isinstance(result, int):  
-        res_string += str(result)
-    else:
-        res_string += result.capitalize()
 
-print(f"Your ticket {res_string} is a winning ticket!")
+def ticket_combination():
+    res_string=""
+    for r in range(0,4):
+        result = choice(list)
+        #int or not?
+        if isinstance(result, int):  
+            res_string += str(result)
+        else:
+            res_string += result.capitalize()
+    return res_string
+
+
+
+def print_winner_ticket(res_string):
+    print(f"Your ticket {res_string} is a winning ticket!")
+
+res_string = ticket_combination()
+print_winner_ticket(res_string)
