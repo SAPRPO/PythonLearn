ac4987adf6ec6258c3c8e95f9efd0c404f70e84c
Ilya Uryupin <i.uryupin@module.ru>
2025-05-27 14:38:12 +0300

    update text file, add read from file

--- 10_files_exceptions/examples/readfromfile/pi_string.py
+++ 10_files_exceptions/examples/readfromfile/pi_string.py
@@ -1,1 +1,15 @@
 from pathlib import Path
+
+relative_filepath= '10_files_exceptions/examples/readfromfile/'
+path = Path(f'{relative_filepath}/pi_digits.txt')
+
+contents = path.read_text()
+
+lines = contents.splitlines()
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()  #strip() - removing spaces
+
+print(pi_string)
+print(len(pi_string)) 
+
