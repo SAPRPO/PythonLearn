9b6ac08181223269cb96234d364d7e0a89c2fe45
Ilya Uryupin <uis071287@gmail.com>
2025-05-28 09:48:46 +0300

    update count 4

--- 10_files_exceptions/tasks/writetofile/guest.py
+++ 10_files_exceptions/tasks/writetofile/guest.py
@@ -10,7 +10,7 @@ print("----------10.5---------")
 path = Path(f"{relative_filepath}guest_book.txt")
 count = 0
 name = ''
-while count < 5:
+while count < 4:
     name += input("Hello! What is your name?\n") + '\n'
     count = count+1
 path.write_text(name)
