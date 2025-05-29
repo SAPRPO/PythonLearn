43a5e07abb80a6bab9f633fc4ab712ca7b1a5948
Ilya Uryupin <uis071287@gmail.com>
2025-05-29 14:19:43 +0300

    update exercise 'popular words'

--- 10_files_exceptions/tasks/exceptions/ex_10_10_popular_words.py
+++ 10_files_exceptions/tasks/exceptions/ex_10_10_popular_words.py
@@ -1,3 +1,25 @@
 #find popular words in text!
 from pathlib import Path
 
+relative_filepath = '10_files_exceptions/tasks/exceptions/'
+path = Path(f"{relative_filepath}S.txt")
+
+try:
+    contents = path.read_text(encoding='utf-8')
+except Exception:
+    print(f"Error open!")
+else:
+    print(f"{path.name} successfully read")
+
+keyword = 'командир'
+
+lines = contents.splitlines()
+repeat = 0
+string_index=1
+for line in lines:
+    count_word= line.lower().count(keyword)
+    repeat += count_word
+    string_index =string_index+1
+
+
+print(f"Keyword '{keyword}' repeated {repeat} times")
