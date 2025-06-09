0e1aa3e0d576370de1eff9756395f70280b98b58
Ilya Uryupin <uis071287@gmail.com>
2025-06-09 18:23:54 +0300

    add while in rw

--- 12_Project_chapter_15/examples/rw_visual.py
+++ 12_Project_chapter_15/examples/rw_visual.py
@@ -3,12 +3,15 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 rw = RandomWalk() #5000 as default
-rw.fill_walk()
+while True:
+    rw.fill_walk()
 
-#нанесение точек на диаграмму
-plt.style.use(style='classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-ax.set_aspect('equal') #обе оси имеют равное расстояние между делениями
-plt.show()
-#360
+    #нанесение точек на диаграмму
+    plt.style.use(style='classic')
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=15)
+    ax.set_aspect('equal') #обе оси имеют равное расстояние между делениями
+    plt.show()
+    keep_running=input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
