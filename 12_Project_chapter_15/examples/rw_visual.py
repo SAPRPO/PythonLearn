1c40e454370eb5c13da40289bfae9b1b6304925f
Ilia Uryupin <uis071287@gmail.com>
2025-06-10 05:06:17 +0300

    update rw_visual

--- 12_Project_chapter_15/examples/rw_visual.py
+++ 12_Project_chapter_15/examples/rw_visual.py
@@ -8,10 +8,19 @@ while True:
 
     #нанесение точек на диаграмму
     plt.style.use(style='classic')
-    fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, s=15)
+    #Изменение размера заполнения экрана
+    fig, ax = plt.subplots(figsize=(16,9)) #fig - весь рисунок, ax, одна из диаграмм
+    #форматирование 
+    point_numbers= range(rw.num_points) #генерирование списка чисел
+    ax.scatter(rw.x_values, rw.y_values,c = point_numbers,  cmap = plt.cm.Blues, edgecolor='none', s=15) #test add
     ax.set_aspect('equal') #обе оси имеют равное расстояние между делениями
+    #выделение первой и последней точки
+    ax.scatter(0,0,c='green', edgecolor='none', s=100) # 1-я точка
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolor='none', s =100) #последняя точка
+    #Удаление осей
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
     plt.show()
-    keep_running=input("Make another walk? (y/n): ")
+    keep_running=input("Make another walk? (y/n): ") #add try/except for y/n
     if keep_running == 'n':
         break
