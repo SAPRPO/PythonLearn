bd19952b05855a03722c959c9852361bd364f033
Ilia Uryupin <uis071287@gmail.com>
2025-06-11 00:19:24 +0300

    rename, add ex 15.6 fom graphs

---
+++ 12_Project_chapter_15/tasks/dice_visual_2d8.py
@@ -0,0 +1,27 @@
+from die import Die
+import plotly.express as px
+
+die1 = Die(8)
+die2 = Die(8) 
+
+results = []
+#выборка
+for roll_num in range(10000):
+    res = die1.roll() + die2.roll()
+    results.append(res)
+#print(results)
+#частота
+frequencies = []
+max_result = die1.num_sides + die2.num_sides
+poss_results = range(2, max_result+1)
+
+for value in poss_results:
+    freq = results.count(value)
+    frequencies.append(freq)
+
+#Визуализация
+title = "Results of Rolling two D8 1000 times"
+labels ={'x': 'Result', 'y': 'Frequency of Result'}
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+fig.update_layout(xaxis_dtick=1)
+fig.show()
