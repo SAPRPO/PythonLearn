cd0632d29ff835f97c87ddd1ff45cb9d8876e4e4
Ilya Uryupin <uis071287@gmail.com>
2025-06-11 11:22:36 +0300

    remove test comment

--- 12_Project_chapter_15/examples/die.py
+++ 12_Project_chapter_15/examples/die.py
@@ -2,7 +2,7 @@ from random import randint
 
 class Die:
     """Класс, представляющий один кубик""" 
-    def __init__(self, num_sides=6): #test
+    def __init__(self, num_sides=6): 
         self.num_sides=num_sides
     
     def roll(self):
