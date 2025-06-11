9309f7d861a4b86e233c60d2abeb053827fe6d04
Ilia Uryupin <uis071287@gmail.com>
2025-06-11 16:01:44 +0300

    add 15.7, three cubes

---
+++ 12_Project_chapter_15/tasks/cube_three.py
@@ -0,0 +1,30 @@
+#three D6 cube
+from die import Die
+import plotly.express as px
+
+die1 = Die()
+die2 = Die()
+die3 = Die()
+
+results =[]
+
+for roll_num in range(1000):
+    result = die1.roll() + die2.roll() + die3.roll()
+    results.append(result)
+
+frequencies = []
+max_result = die1.num_sides +die2.num_sides +die3.num_sides
+poss_results = range(3,max_result+1)
+
+for value in poss_results:
+    freq = results.count(value)
+    frequencies.append(freq)
+
+
+#Визуализация
+title = "Results of Rolling two D8 1000 times"
+labels ={'x': 'Result', 'y': 'Frequency of Result'}
+ 
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+fig.update_layout(xaxis_dtick=1)
+fig.show()
