0064b9d96b3ab74409ba2dbb133112db41bd5dc9
Ilya Uryupin <uis071287@gmail.com>
2025-06-25 18:33:35 +0300

    add 2 csv, update ex16.2

---
+++ 13_Project_chapter_16/tasks/compare_sitka_death_valley.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+
+from whoami import Whoami 
+import csv
+
+p = Whoami.get_path()
+relative_filepath = f'{p}/13_Project_chapter_16/'
