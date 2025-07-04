79cf94db6b71572f0c910c3fe777788fdce44d0e
Ilia Uriupin <uis071287@gmail.com>
2025-07-04 16:08:26 +0300

    update eq_world_map, add task1

--- 13_Project_chapter_16/eq_data/exercise/eq_world_map.py
+++ 13_Project_chapter_16/eq_data/exercise/eq_world_map.py
@@ -25,14 +25,10 @@ print(len(all_eq_dicts))
 #magnitude, latitude, longitude
 mags, lons, lats, eq_titles = [], [], [], []
 for eq_dict in all_eq_dicts:
-    mag = eq_dict['properties']['mag']
-    lon = eq_dict['geometry']['coordinates'][0]
-    lat = eq_dict['geometry']['coordinates'][1]
-    eq_title = eq_dict['properties']['title']
-    mags.append(mag)
-    lons.append(lon)
-    lats.append(lat)
-    eq_titles.append(eq_title)
+    mags.append(eq_dict['properties']['mag'])
+    lons.append(eq_dict['geometry']['coordinates'][0])
+    lats.append(eq_dict['geometry']['coordinates'][1])
+    eq_titles.append(eq_dict['properties']['title'])
 
 
 #diagramm
@@ -40,7 +36,7 @@ for eq_dict in all_eq_dicts:
 title = 'Global Earthquakes'
 fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title=title,
                      color = mags,
-                     color_continuous_scale='agsunset',
+                     color_continuous_scale='Agsunset',
                      labels={'color':'Magnitude'},
                      projection='natural earth',
                      hover_name=eq_titles,
