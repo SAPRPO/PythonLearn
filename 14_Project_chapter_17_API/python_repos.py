64a4cc8cd7914f276e35ed84c2bc2080436832d8
Ilia Uryupin <uis071287@gmail.com>
2025-07-09 06:25:37 +0300

    Chapter 17_API request

---
+++ 14_Project_chapter_17_API/python_repos.py
@@ -0,0 +1,25 @@
+import requests
+from samba.dcerpc.dcerpc import response
+
+#Create API request
+
+url = "https://api.github.com/search/repositories" #строка
+url += "?q=language:python+sort:stars+stars:>10000" #запрос
+
+headers = {"Accept": "application/vnd.github.v3+json"}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+response_dict = r.json() #преобразование объекта в словарь
+
+print(response_dict.keys())
+print(f"Total repositories: {response_dict['total_count']}")
+print(f"Complete results: {not response_dict['incomplete_results']}")
+#Анализ информации о репозиториях
+repo_dicts =response_dict['items']
+print(f"Repositories returned: {len(repo_dicts)}")
+#Анализ первого репозитория
+repo_dict = repo_dicts[0]
+print(f"\nKeys: {len(repo_dict)}")
+for key in sorted(repo_dict.keys()):
+    print(key)
+#409
